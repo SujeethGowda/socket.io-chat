276bf045270ad657623677b53154f61ca4cabc54
Sujeeth Gowda <you@example.com>
2019-09-11 11:33:53 +0530

    Inistial commit
    Insitial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//App Setup
+
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('Listening to requests on port 4000');
+});
+
+app.use(express.static('public'));
+
+
+//socket 
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('made socket connection',socket.id);
+
+    socket.on('chat', function(data){
+        io.sockets.emit('chat',data);
+    });
+
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing',data);
+    });
+});
